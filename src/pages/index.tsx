f7d1b06d31a1cf6a73464dc86f7412f64d919a36
Wayne Van Son <waynevanson@gmail.com>
2024-02-11 17:26:37 +1100

    feat: add introductory text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,16 @@ import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 
 export default function IndexPage(props: PageProps) {
-  return <section />
+  return (
+    <section>
+      <p>Hello! Welcome to my partner's website.</p>
+      <p>
+        She makes delicious food, and this website is a mix between me applying
+        my technical skills and the fear of losing her without her recipes being
+        passed on.
+      </p>
+    </section>
+  )
 }
 
 export const Head: HeadFC = () => <title>Home Page</title>
